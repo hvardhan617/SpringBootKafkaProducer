7240a1ee5fab968ada9b0d5939ec863bd6e3dccb
Harshavardhan P <harshavardhan.p@intellectdesign.com>
2018-05-29 13:01:25 +0530

    Initial import

--- src/main/java/com/example/demo/SpringKafkaProducerApplication.java
+++ src/main/java/com/example/demo/SpringKafkaProducerApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*(Reference--https://www.codenotfound.com/spring-kafka-consumer-producer-example.html)
+*/
 @SpringBootApplication
 public class SpringKafkaProducerApplication {
 
